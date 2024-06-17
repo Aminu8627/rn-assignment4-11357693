73ddc445b90523c4cba5c61c7fb7ef7e7e687d66
Aminu8627 <maumar003@st.ug.edu.gh>
2024-06-17 15:44:28 +0000

    Login Page created

---
+++ Assignment3/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View,Button,Image, Pressable } from 'react-native';
+
+
+function Homepage() {
+  return (
+    <View>
+      <Text>Eric Atsu</Text>
+    </View>
+  )
+}
+
+export default Homepage
+
+
